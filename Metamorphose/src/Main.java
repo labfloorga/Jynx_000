20b33ecc72f2a7f4e669a80884312eabe74c87ec
ib <ib@WINIB-TGQ1AECAM>
2019-06-27 14:40:33 +0200

    commit flo

--- Metamorphose/src/Main.java
+++ Metamorphose/src/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World !!");
-<<<<<<< HEAD
+
 		System.out.println("ffff");
 		System.out.println("coucou");
-=======
+
      int a = 1 ; 
->>>>>>> branch 'master' of https://github.com/labfloorga/Jynx_000.git
+ branch 'master' of https://github.com/labfloorga/Jynx_000.git
 	}
 
 }
